ae6f3c23f1d90da360d39f079d54171b0a69ebc3
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2021-08-05 13:54:09 -0400

    create parser class

---
+++ Parser.py
@@ -0,0 +1,9 @@
+'''
+VidMob Take-Home Parser Class
+Isaac Sparks-Willey
+August 5, 2021
+
+This class is used to parse the calculator's CLI input, validating it's syntax
+and converting the single line string into a list of numbers and operators.
+'''
+class Parser:
