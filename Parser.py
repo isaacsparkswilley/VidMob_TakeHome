a4ec791f8a94431b0b173711f07cce4da7a19010
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2021-08-05 14:07:30 -0400

    add regex globals and parseInput skeleton

--- Parser.py
+++ Parser.py
@@ -6,4 +6,19 @@ August 5, 2021
 This class is used to parse the calculator's CLI input, validating it's syntax
 and converting the single line string into a list of numbers and operators.
 '''
+import re
 class Parser:
+
+    # regex to use for validating numbers
+    numRegex = re.compile('-?(?:\d*\.\d+|\d+)')
+
+    # regex to use for validating operators
+    opRegex = re.compile('[\+\-]')
+
+    '''
+    DESC: Parse input string into a list of numbers and operators.
+    IN: (String) Input from calculator CLI
+    OUT: (List) List of seperated numbers and operators
+    '''
+    def parseInput(inputString):
+        pass
