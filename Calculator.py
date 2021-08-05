cbafa51e7dfb6b9214a6f7682ec44ec07dbf0e96
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2021-08-05 15:18:00 -0400

    add Calculator class and calculate method skeleton

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+'''
+VidMob Take-Home Calculator Class
+Isaac Sparks-Willey
+August 5, 2021
+
+This class is used to calculate a value based on the output of the parser.
+'''
+import operator
+class Calculator:
+
+    def calculate(inputList):
+        pass
