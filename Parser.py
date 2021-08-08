6459e57352301a03e7e207f04c71b5493c9b4e15
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2021-08-08 13:18:34 -0400

    add spacing to Parser.parseInput()

--- Parser.py
+++ Parser.py
@@ -9,8 +9,8 @@ class Parser:
     '''
     Parser
     
-    This class is used to parse the calculator's CLI input, validating it's syntax and converting the string into
-    a list of numbers and operators.
+    This class is used to parse the calculator's CLI input, validating it's syntax and converting the string
+    into a list of numbers and operators.
 
     Class Variables
     ---------------
@@ -62,45 +62,59 @@ class Parser:
         while inputString != '':
             # get open parens (which will always come before a number)
             match = re.match(Parser.openParenRegex, inputString)
+            
             while match is not None:
                 # note number of opened parentheses
                 openParens += 1
+                
                 # add paren to out
                 out.append(match.group())
+                
                 # remove paren from inputString
                 inputString = inputString.replace(match.group(), "", 1)
+                
                 # check for another paren
                 match = re.match(Parser.openParenRegex, inputString)
 
             # get next number
             match = re.match(Parser.numRegex, inputString)
+            
             if match is not None:
                 # cast number to float, add to out
                 out.append(float(match.group()))
+                
                 # remove number from inputString
                 inputString = inputString.replace(match.group(), "", 1)
+
             else:
                 raise SyntaxError("Invalid Input")
 
             # get close parens (which will always come after a number)
             match = re.match(Parser.closeParenRegex, inputString)
+            
             while match is not None:
                 # note number of closed parentheses
                 openParens -= 1
+                
                 # add paren to out
                 out.append(match.group())
+                
                 # remove paren from inputString
                 inputString = inputString.replace(match.group(), "", 1)
+                
                 # check for another paren
                 match = re.match(Parser.closeParenRegex, inputString)
 
             # get next operator
             match = re.match(Parser.opRegex, inputString)
+            
             if match is not None:
                 # add operator to out
                 out.append(match.group())
+                
                 # remove operator from input string
                 inputString = inputString.replace(match.group(), "", 1)
+                
             elif inputString != '':
                 raise SyntaxError("Invalid Syntax")
 
