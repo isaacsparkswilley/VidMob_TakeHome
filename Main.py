e1c534bcc9ef5534b961bdca99cb3b954ed31c8e
Isaac Sparks-Willey <isaac.sparkswilley@maine.edu>
2021-08-09 10:18:17 -0400

    define main script

--- Main.py
+++ Main.py
@@ -5,3 +5,35 @@ August 9, 2021
 
 This is the main script for running the calculator program.
 '''
+from Parser import *
+from Calculator import *
+
+# main loop
+while True:
+    # get user input
+    inputString = input("Please enter an operation to be performed. (Exit to exit program)\n")
+
+    # exit program on "exit" input
+    if inputString.lower() == 'exit':
+        break
+
+    # attempt to parse inputString
+    try:
+        inputList = Parser.parseInput(inputString)
+
+    # print syntax errors (malformed input)
+    except SyntaxError as e:
+        print(str(e) + "\n")
+        continue
+
+    # attempt to calculate result
+    try:
+        result = Calculator.calculate(inputList)
+
+    # catch divide by zero errors
+    except ZeroDivisionError:
+        print("Unable to divide by zero.\n")
+        continue
+
+    # print result
+    print("Result: " + str(result) + "\n")
